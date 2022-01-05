224a9eb7d75586ddd9d3fa816ca26ad5557acc6b
Maxim Byts <maxim.90svet@gmail.com>
2022-01-05 20:05:08 +0300

    Refactor test for Model

--- src/scripts/Model.test.ts
+++ src/scripts/Model.test.ts
@@ -1,5 +1,4 @@
 import Model from './Model';
-import Generator from './Generator';
 import { Configuration } from './Types';
 import Observer from './Observer';
 import { ALPHABET } from './Constants';
@@ -8,129 +7,186 @@ class ObserverMock extends Observer {
   update() {};
 }
 
-describe("Testing for range type configuration", () => {
+describe("Testing 'Model' for range type configuration:\n", () => {
+  const points: Array<number> = [1, 2.5, 5, 10];
   const config: Partial<Configuration> = {
     type: 'range',
     range: [0, 10],
-    points: [1, 2.5, 5, 10],
     step: 2,
+    points,
   };
   const model = new Model(config);
   const observer = new ObserverMock();
   const observerUpdateSpy = jest.spyOn(observer, 'update');
   model.attach(observer);
 
-  it("Should return the point value as a percentage", () => {
-    expect(model.getPointLocationOnScale(0)).toStrictEqual(10);
-    expect(model.getPointLocationOnScale(1)).toStrictEqual(25);
-    expect(model.getPointLocationOnScale(2)).toStrictEqual(50);
-    expect(model.getPointLocationOnScale(3)).toStrictEqual(100);
+  it("The 'getPointValue' method should return the point value", () => {
+    points.forEach((val, i) => {
+      expect(model.getPointValue(i)).toStrictEqual(val);
+    });
   });
 
-  it("Should return the point value", () => {
-    expect(model.getPointValue(0)).toStrictEqual(1);
-    expect(model.getPointValue(1)).toStrictEqual(2.5);
-    expect(model.getPointValue(2)).toStrictEqual(5);
-    expect(model.getPointValue(3)).toStrictEqual(10);
+  it("The 'getPointValues' method should return an array of all point values", () => {
+    expect(model.getPointValues()).toStrictEqual(points);
   });
 
-  it("Should return the distance from point to borders", () => {
-    expect(model.getDistanceToBorders(0)).toStrictEqual([1, 1.5]);
-    expect(model.getDistanceToBorders(1)).toStrictEqual([1.5, 2.5]);
-    expect(model.getDistanceToBorders(2)).toStrictEqual([2.5, 5]);
-    expect(model.getDistanceToBorders(3)).toStrictEqual([5, 0]);
+  const pointScale = [10, 25, 50, 100];
+  it("The 'getPointLocationOnScale' method should return the point value as a percentage", () => {
+    points.forEach((_, i) => {
+      expect(model.getPointLocationOnScale(i)).toStrictEqual(pointScale[i]);
+    });
   });
 
-  it("Should return an array of all points values in percentage", () => {
-    expect(model.getPointScale()).toStrictEqual([10, 25, 50, 100]);
+  it("The 'getPointScale' method should return an array of all points values as a percentage", () => {
+    expect(model.getPointScale()).toStrictEqual(pointScale);
   });
 
-  it("Should return an array of all points values", () => {
-    expect(model.getPointValues()).toStrictEqual([1, 2.5, 5, 10]);
+  const distances = [1, 1.5, 2.5, 5, 0];
+  it("The 'getDistanceToBorders' method should return the distance from point to borders", () => {
+    points.forEach((_, i) => {
+      expect(model.getDistanceToBorders(i)).toStrictEqual(distances.slice(i, i + 2));
+    });
   });
 
-  it("Should return all distances between points include the min and max border", () => {
-    expect(model.getDistances()).toStrictEqual([1, 1.5, 2.5, 5, 0]);
+  it("The 'getDistances' method should return all distances between points (include min and max)", () => {
+    expect(model.getDistances()).toStrictEqual(distances);
   });
 
-  it("Should set a point when new point value match borders", () => {
-    expect(model.setPointValue(0, 2.6)).toBeFalsy();
-    expect(model.setPointValue(0, -1)).toBeFalsy();
-    expect(model.setPointValue(0, 1.5)).toBeTruthy();
-    expect(model.getPointValue(0)).toStrictEqual(1.5);
-    expect(model.setPointValue(0, 1)).toBeTruthy();
-    expect(model.getPointValue(0)).toStrictEqual(1);
-    expect(observerUpdateSpy).toBeCalledTimes(2);
+  describe("Testing the 'setPoint' method:\n", () => {
+    it("Should return false when point value isn't within the borders", () => {
+      expect(model.setPointValue(0, -1)).toBeFalsy();
+      expect(model.setPointValue(0, 3)).toBeFalsy();
+      expect(model.setPointValue(3, 4)).toBeFalsy();
+      expect(model.setPointValue(3, 11)).toBeFalsy();
+    });
+
+    it("Should set point and return true when point value is within the borders", () => {
+      expect(model.setPointValue(0, 2)).toBeTruthy();
+      expect(model.getPointValue(0)).toStrictEqual(2);
+      expect(observerUpdateSpy).toBeCalledTimes(1);
+      model.resetStateToInitial();
+    });
   });
 
-  it("Should set a min border", () => {
-    expect(model.setMinBorder(1.1)).toBeFalsy();
-    expect(model.setMinBorder(1)).toBeTruthy();
-    expect(model.getMinBorder()).toStrictEqual(1);
-    expect(model.setMinBorder(0)).toBeTruthy();
-    expect(model.getMinBorder()).toStrictEqual(0);
-    expect(observerUpdateSpy).toBeCalledTimes(2);
+  describe("Testing the 'setMinBorder' method:\n", () => {
+    it("Should return false when value not finite", () => {
+      expect(model.setMinBorder(-Infinity)).toBeFalsy();
+      expect(model.setMinBorder(NaN)).toBeFalsy();
+    });
+
+    it("Should return false when value greater than the first point value", () => {
+      expect(model.setMinBorder(2.6)).toBeFalsy();
+    });
+
+    it("Should return false when value equal to maxBorder value", () => {
+      model.setPointValue(2, 10);
+      model.setPointValue(1, 10);
+      model.setPointValue(0, 10);
+      expect(model.setMinBorder(10)).toBeFalsy();
+      model.resetStateToInitial();
+    });
+
+    it("Should set min border and return true when value is within the borders", () => {
+      expect(model.setMinBorder(1)).toBeTruthy();
+      expect(model.getMinBorder()).toStrictEqual(1);
+      expect(observerUpdateSpy).toBeCalledTimes(1);
+      model.resetStateToInitial();
+    });
   });
 
-  it("Should set a max border", () => {
-    expect(model.setMaxBorder(0.9)).toBeFalsy();
-    expect(model.setMaxBorder(20)).toBeTruthy();
-    expect(model.getMaxBorder()).toStrictEqual(20);
-    expect(model.getPointScale()).toStrictEqual([5, 12.5, 25, 50]);
-    expect(model.setMaxBorder(10)).toBeTruthy();
-    expect(model.getMaxBorder()).toStrictEqual(10);
-    expect(observerUpdateSpy).toBeCalledTimes(2);
+  describe("Testing the 'setMaxBorder' method:\n", () => {
+    it("Should return false when value not finite", () => {
+      expect(model.setMaxBorder(Infinity)).toBeFalsy();
+      expect(model.setMaxBorder(NaN)).toBeFalsy();
+    });
+
+    it("Should return false when value equal to minBorder value", () => {
+      model.setPointValue(0, 0);
+      model.setPointValue(1, 0);
+      model.setPointValue(2, 0);
+      model.setPointValue(3, 0);
+      expect(model.setMaxBorder(0)).toBeFalsy();
+      model.resetStateToInitial();
+    });
+
+    it("Should set max border and return true when value is within the borders", () => {
+      console.log(model.getPointValues())
+      expect(model.setMaxBorder(15)).toBeTruthy();
+      expect(model.getMaxBorder()).toStrictEqual(15);
+      expect(observerUpdateSpy).toBeCalledTimes(1);
+      model.resetStateToInitial();
+    });
   });
 
-  it("Should set a step", () => {
-    expect(model.setStep(11)).toBeFalsy();
-    expect(model.setStep(10)).toBeTruthy();
-    expect(model.getStep()).toStrictEqual(10);
-    expect(model.setStep(1)).toBeTruthy();
-    expect(model.getStep()).toStrictEqual(1);
-    expect(observerUpdateSpy).toBeCalledTimes(2);
+  describe("Testing the 'setStep' method:\n", () => {
+    it("Should return false when value greater than the range length", () => {
+      const rangeLength = 10;
+      expect(model.setStep(rangeLength + 0.001)).toBeFalsy();
+    });
+
+    it("Should return false when value is zero", () => {
+      expect(model.setStep(0)).toBeFalsy();
+    });
+
+    it("Should return false when value is negative", () => {
+      expect(model.setStep(-1)).toBeFalsy();
+    });
+
+    it("Should set step and return true when value is within the borders", () => {
+      expect(model.setStep(5)).toBeTruthy();
+      expect(model.getStep()).toStrictEqual(5);
+      expect(observerUpdateSpy).toBeCalledTimes(1);
+      model.resetStateToInitial();
+    });
   });
 });
 
-describe("Testing for set type configuration", () => {
+describe("Testing 'Model' for set type configuration:\n", () => {
+  const points = [5, 10, 15];
   const config: Partial<Configuration> = {
     type: 'set',
     set: [...ALPHABET],
-    points: [5, 10, 15],
     step: 1,
+    points,
   };
   const model = new Model(config);
   const observer = new ObserverMock();
   const observerUpdateSpy = jest.spyOn(observer, 'update');
   model.attach(observer);
 
-  it("Should return the point value", () => {
-    expect(model.getPointValue(0)).toStrictEqual(5);
-    expect(model.getPointValue(1)).toStrictEqual(10);
-    expect(model.getPointValue(2)).toStrictEqual(15);
+  it("The 'getPointValue' method should return the point value", () => {
+    points.forEach((val, i) => {
+      expect(model.getPointValue(i)).toStrictEqual(val);
+    });
   });
 
-  it("Should return an array of all point values", () => {
-    expect(model.getPointValues()).toStrictEqual([5, 10, 15]);
+  it("The 'getPointValues' method should return an array of all point values", () => {
+    expect(model.getPointValues()).toStrictEqual(points);
   });
 
-  it("Should return the point value as a percentage", () => {
-    expect(model.getPointLocationOnScale(0)).toStrictEqual(20);
-    expect(model.getPointLocationOnScale(1)).toStrictEqual(40);
-    expect(model.getPointLocationOnScale(2)).toStrictEqual(60);
+  const pointScale = [20, 40, 60];
+  it("The 'getPointLocationOnScale' method should return the point value as a percentage", () => {
+    points.forEach((_, i) => {
+      expect(model.getPointLocationOnScale(i)).toStrictEqual(pointScale[i]);
+    });
   });
 
-  it("Should return an array of all point values as a percentage", () => {
-    expect(model.getPointScale()).toStrictEqual([20, 40, 60]);
+  it("The 'getPointScale' method should return an array of all point values as a percentage", () => {
+    expect(model.getPointScale()).toStrictEqual(pointScale);
   });
 
-  it("Should return the distance from point to borders", () => {
-    expect(model.getDistanceToBorders(0)).toStrictEqual([5, 5]);
-    expect(model.getDistanceToBorders(1)).toStrictEqual([5, 5]);
-    expect(model.getDistanceToBorders(2)).toStrictEqual([5, 10]);
+  const distances = [5, 5, 5, 10];
+  it("The 'getDistanceToBorders' method should return the distance from point to borders", () => {
+    points.forEach((_, i) => {
+      expect(model.getDistanceToBorders(i)).toStrictEqual(distances.slice(i, i + 2));
+    });
   });
 
-  describe("Testing the 'setPoint' method", () => {
+  it("The 'getDistances' method should return all distances between points (include min and max)", () => {
+    expect(model.getDistances()).toStrictEqual(distances);
+  });
+
+  describe("Testing the 'setPoint' method:\n", () => {
     it("Should return false when point value isn't within the borders", () => {
       expect(model.setPointValue(0, -1)).toBeFalsy();
       expect(model.setPointValue(0, 11)).toBeFalsy();
@@ -148,7 +204,7 @@ describe("Testing for set type configuration", () => {
     model.resetStateToInitial();
   });
 
-  describe("Testing the 'setMinBorder' method", () => {
+  describe("Testing the 'setMinBorder' method:\n", () => {
     it("Should return false when value less than the first set index", () => {
       expect(model.setMinBorder(-1)).toBeFalsy();
     });
@@ -173,7 +229,7 @@ describe("Testing for set type configuration", () => {
     });
   });
 
-  describe("Testing the 'setMaxBorder' method", () => {
+  describe("Testing the 'setMaxBorder' method:\n", () => {
     it("Should return false when value greater than the last set index", () => {
       expect(model.setMaxBorder(26)).toBeFalsy();
     });
@@ -198,9 +254,13 @@ describe("Testing for set type configuration", () => {
     });
   });
 
-  describe("Testing the 'setStep' method", () => {
+  describe("Testing the 'setStep' method:\n", () => {
     it("Should return false when value greater than the number of elements in the set", () => {
       expect(model.setStep(ALPHABET.length)).toBeFalsy();
+    });
+
+    it("Should return false when value is zero", () => {
+      expect(model.setStep(0)).toBeFalsy();
     });
 
     it("Should return false when value is negative", () => {
