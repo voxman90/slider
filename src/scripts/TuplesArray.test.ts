5b75e98fdfb17bcb114fbe7bd04c94bf4b24a9ea
Maxim Byts <maxim.90svet@gmail.com>
2022-01-14 23:26:23 +0300

    Add TuplesArray

---
+++ src/scripts/TuplesArray.test.ts
@@ -0,0 +1,41 @@
+import { createTuplesArray, removeTuplesWithDuplicateKeys } from "./TuplesArray";
+import { NonBottomValue } from "./Types";
+
+type Tuple = [string, NonBottomValue];
+
+let tuplesArray: Array<Tuple>;
+
+beforeEach(() => {
+  const generatingFunc = (i: number): Tuple => [`${i}`, i];
+  tuplesArray = createTuplesArray(generatingFunc, 5, "test");
+})
+
+describe("Test 'removeTuplesWithDuplicateKeys' utility", () => {
+  test("case 1", () => {
+    const doubledTuplesArray = [...tuplesArray, ...tuplesArray];
+    expect(removeTuplesWithDuplicateKeys(doubledTuplesArray)).toEqual(tuplesArray);
+  });
+
+  test("case 2", () => {
+    const tuplesArray: Array<Tuple> = [
+      ["1", 1],
+      ["1", 2],
+      ["3", 1],
+      ["1", 3],
+      ["2", 1],
+      ["2", 2],
+      ["4", 1],
+    ];
+    const controlTuplesArray = [
+      ["1", 1],
+      ["3", 1],
+      ["2", 1],
+      ["4", 1],
+    ];
+    expect(removeTuplesWithDuplicateKeys(tuplesArray)).toEqual(controlTuplesArray);
+  });
+
+  test("case 3", () => {
+    expect(removeTuplesWithDuplicateKeys([])).toEqual([]);
+  });
+});
---
+++ src/scripts/TuplesArray.ts
@@ -0,0 +1,24 @@
+import { NonBottomValue } from "./Types";
+
+type Tuple = [string, NonBottomValue];
+type TuplesArray = Array<Tuple>;
+
+function createTuplesArray(generatingFunc: (i: number) => Tuple, length: number, prefix: string = ""): TuplesArray {
+  const tuplesArray: TuplesArray = [];
+
+  for (let i = 0; i < length; i += 1) {
+    const [tupleKey, tupleValue] = generatingFunc(i);
+    tuplesArray.push([`${prefix}${tupleKey}`, tupleValue]);
+  }
+
+  return tuplesArray;
+}
+
+function removeTuplesWithDuplicateKeys(tuplesArray: TuplesArray): Array<Tuple> {
+  const seen: Set<string> = new Set();
+  return tuplesArray.filter(([tupleKey, _]) => {
+    return seen.has(tupleKey) ? false : (seen.add(tupleKey), true);
+  });
+}
+
+export { createTuplesArray, removeTuplesWithDuplicateKeys };
